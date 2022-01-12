1c0c7845b474b156eee20378b143d207e5b080e3
emanuel caro <darkemlord@gmail.com>
2022-01-12 17:28:54 +0900

    product cards created

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
-import { Heading, Page } from "@shopify/polaris";
+import {Page,Layout,Card, Tag} from "@shopify/polaris";
+const Index = () => {
+  return <Page title="Product Tags">
+    <Layout>
+      <Layout.Section oneHalf>
+        <Card title="Product Name">
+          <Card.Section>
+            <Tag  onRemove={()=>console.log("Heloo")}>Test</Tag>
+            <Tag  onRemove={()=>console.log("Good")}>Good</Tag>
+          </Card.Section>
 
-const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
+          <Card.Section>
+            <p>
+              View a summary of your online store’s performance, including sales,
+              visitors, top products, and referrals.
+            </p>
+          </Card.Section>
+        </Card>
+      </Layout.Section>
+      <Layout.Section oneHalf>
+       <Card title="Product Name" sectioned>
+         <p>Simple Texts</p>
+
+       </Card>
+      </Layout.Section>
+    </Layout>
   </Page>
-);
-
+};
 export default Index;
