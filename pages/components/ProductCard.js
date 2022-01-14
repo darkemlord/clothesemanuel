d3c4545a1ac58fd345a3b0b4723cd74373abbe20
emanuel caro <darkemlord@gmail.com>
2022-01-14 18:44:48 +0900

    graphql query updated

--- pages/components/ProductCard.js
+++ pages/components/ProductCard.js
@@ -1,12 +1,42 @@
-import  React, { useState, useCallback } from 'react';
-import { Layout,Card, Stack, TextField, Button } from '@shopify/polaris';
+import React, { useState, useCallback } from "react";
+import { Layout, Card, Stack, TextField, Button } from "@shopify/polaris";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const GET_PRODUCTS_BY_ID = gql`
+  query getProducts($ids: [ID!]!) {
+    nodes(ids: $ids) {
+      ... on Product {
+        title
+        handle
+        descriptionHtml
+        id
+        images(first: 1) {
+          edges {
+            node {
+              id
+              altText
+            }
+          }
+        }
+        variants(first: 1) {
+          edges {
+            node {
+              price
+              id
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 const ProductCard = (props) => {
-
-  const [value, setValue] = useState('');
-  const [newPrice, setNewPrice] = useState('');
+  const [value, setValue] = useState("");
+  const [newPrice, setNewPrice] = useState("");
   const handleChange = useCallback((newValue) => setValue(newValue), []);
-  const handleButton = () => setNewPrice(value)
+  const handleButton = () => setNewPrice(value);
 
   return (
     <Layout.Section oneHalf>
@@ -20,12 +50,14 @@ const ProductCard = (props) => {
                 <p>Price: {newPrice ? newPrice : props.price}¥</p>
                 <Stack>
                   <TextField
-                      value={value}
-                      type="number"
-                      onChange={handleChange}
-                      autoComplete="off"
+                    value={value}
+                    type="number"
+                    onChange={handleChange}
+                    autoComplete="off"
                   />
-                  <Button primary onClick={handleButton}>update price</Button>
+                  <Button primary onClick={handleButton}>
+                    update price
+                  </Button>
                 </Stack>
               </Stack>
             </div>
@@ -33,7 +65,7 @@ const ProductCard = (props) => {
         </Card.Section>
       </Card>
     </Layout.Section>
-  )
-}
+  );
+};
 
 export default ProductCard;
