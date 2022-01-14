9cd7e9d6e6edd17aa0e6d6cb2027c17d071b63c0
emanuel caro <darkemlord@gmail.com>
2022-01-14 09:58:58 +0900

    changin node version

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import {Page, EmptyState} from '@shopify/polaris';
+import React from "react";
+import { Page, EmptyState } from "@shopify/polaris";
 
 export const Dashboard = () => {
   return (
-      <Page>
+    <Page>
       <EmptyState>
-        <h1>Hello there!</h1>
+        <h1>Hello there! Thank you for visiting!</h1>
       </EmptyState>
     </Page>
-
-  )
-}
+  );
+};
 
 export default Dashboard;
